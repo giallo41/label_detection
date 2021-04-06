0e7bc20295c88f9edd0d9cc42dfb3543b765d106
Noah Jung <giallo.hos@gmail.com>
2021-04-06 22:23:54 +0000

    (add/model)

---
+++ train_model.py
@@ -0,0 +1,18 @@
+from imageai.Detection.Custom import DetectionModelTrainer
+
+trainer = DetectionModelTrainer()
+trainer.setModelTypeAsYOLOv3()
+
+data_dir = "./data/images/box/"
+model_dir = "./data/models/pretrained-yolov3.h5"
+trainer.setDataDirectory(data_directory=data_dir)
+
+trainer.setTrainConfig(object_names_array=['true', 'false'], 
+                       batch_size=8, 
+                       num_experiments=100, 
+                       train_from_pretrained_model=model_dir)
+
+
+trainer.trainModel()
+
+print ("---- Train finished --- ")
