73cab759ef1e525fa8e55665753b361cfedba03d
Noah Jung <giallo.hos@gmail.com>
2021-04-10 06:20:44 +0000

    (add/data) add img data file

--- src/data.py
+++ src/data.py
@@ -10,6 +10,30 @@ from sklearn.utils import shuffle as sklearn_shuffle
 from keras.applications.mobilenet_v2 import preprocess_input as mobile_preprocess
 from keras.applications.vgg16 import preprocess_input as vgg16_preprocess
 
+def extract_roi(image, outputs, pixels = 50):
+    image_h, image_w, _ = image.shape
+    
+    roi_list = []
+    for output in outputs:
+        xmin, ymin, xmax, ymax = output['box_points']
+        xmin = max(0, xmin-pixels)
+        ymin = max(0, ymin-pixels)
+        xmax = min(image_w, xmax+pixels)
+        ymax = min(image_h, ymax+pixels)
+        img = image[ymin:ymax,xmin:xmax,:]
+        img = cv2.resize(img, (224,224), interpolation=cv2.INTER_LINEAR)
+        roi_list.append(mobile_preprocess(img))
+    return np.array(roi_list)
+
+def get_file_list(path):
+    file_list = os.listdir(path)
+    if '.ipynb_checkpoints' in file_list:
+        idx = file_list.index('.ipynb_checkpoints')
+        file_list.pop(idx)
+        
+    file_list = [f'{os.path.join(path,file)}' for file in file_list]
+    return file_list
+
 def get_image_value(path, dim, bw, model_type): 
     '''This function will read an image and convert to a specified version and resize depending on which algorithm is being used.  If edge is specified as true, it will pass the img array to get_edged which returns a filtered version of the img'''
     img = image.load_img(path, target_size = dim)
@@ -32,17 +56,17 @@ def get_image_value(path, dim, bw, model_type):
 
 
 def get_data(class_type): 
-    true_paths = [f'./data/images/{class_type}/true/{file}' for file in os.listdir(f'./data/images/{class_type}/true')]
+    true_paths = [f'./data/images/syn/{class_type}/true/{file}' for file in os.listdir(f'./data/images/syn/{class_type}/true')]
     true_labels = [1 for i in range(len(true_paths))]
     
-    false_paths = [f'./data/images/{class_type}/false/{file}' for file in os.listdir(f'./data/images/{class_type}/false')]
+    false_paths = [f'./data/images/syn/{class_type}/false/{file}' for file in os.listdir(f'./data/images/syn/{class_type}/false')]
     false_labels = [0 for i in range(len(false_paths))]
     
     labels = np.array(true_labels + false_labels)
     print(f'{class_type.upper()} Value Counts')
     print(pd.Series(labels).value_counts())
     paths = np.array(true_paths + false_paths)
-    labels = to_categorical(labels)
+    #labels = to_categorical(labels)
     if class_type == 'train':
         paths, labels = sklearn_shuffle(paths, labels)
     return paths, labels
--- train_model.py
+++ train_model.py
@@ -7,7 +7,7 @@ data_dir = "./data/images/box/"
 model_dir = "./data/models/pretrained-yolov3.h5"
 trainer.setDataDirectory(data_directory=data_dir)
 
-trainer.setTrainConfig(object_names_array=['true', 'false'], 
+trainer.setTrainConfig(object_names_array=['true'], 
                        batch_size=8, 
                        num_experiments=100, 
                        train_from_pretrained_model=model_dir)
